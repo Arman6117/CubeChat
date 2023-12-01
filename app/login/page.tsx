19e49f286acc3acbe202aa118068744f6e233a1d
Arman Patel <vanixgaming634@gmail.com>
2023-12-01 20:16:52 +0530

    Login page ui complete

---
+++ app/login/page.tsx
@@ -0,0 +1,16 @@
+import LoginPageContent from "@/components/LoginPageContent";
+import React from "react";
+
+type Props = {};
+
+const page = (props: Props) => {
+  return (
+    <div className="loginBg h-screen w-screen flex items-center justify-center">
+      
+        <LoginPageContent />
+    
+    </div>
+  );
+};
+
+export default page;
---
+++ lib/auth.ts
@@ -0,0 +1,68 @@
+import { UpstashRedisAdapter } from "@next-auth/upstash-redis-adapter";
+import { NextAuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+import { db } from "./db";
+
+function getGoogleCredentials() {
+  const clientId = process.env.GOOGLE_CLIENT_ID;
+  const clientSecret = process.env.GOOGLE_CLIENT_SECRET;
+
+  if (!clientId || clientId.length === 0) {
+    throw new Error("Missing GOOGLE_CLIENT_ID");
+  }
+
+  if (!clientSecret || clientSecret.length === 0) {
+    throw new Error("Missing GOOGLE_CLIENT_SECRET");
+  }
+
+  return { clientId, clientSecret };
+}
+export const authOptions: NextAuthOptions = {
+  adapter:
+    UpstashRedisAdapter(
+      db
+    ) /* //?Adapter is used to take an action after user logins in the app ex: After user login we want to send all the user data to database */,
+  session: {
+    strategy: "jwt",
+  },
+  pages: {
+    signIn: "/login",
+  },
+  providers: [
+    GoogleProvider({
+      clientId: getGoogleCredentials().clientId,
+      clientSecret: getGoogleCredentials().clientSecret,
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, user }) {
+      const dbUser = (await db.get(`user:${token.id}`)) as User | null;
+
+      if (!dbUser) {
+        token.id = user!.id;
+        return token;
+      }
+
+      return {
+        name: dbUser.name,
+        email: dbUser.email,
+        picture: dbUser.image,
+        id: dbUser.id,
+      };
+    },
+    async session({session,token}) {
+        if (token) {
+            session.user.id = token.id;
+            session.user.email = token.email;
+            session.user.image = token.picture
+            session.user.name = token.name;
+        }
+
+        return session;
+    },
+    redirect () {
+        return '/'
+    }
+  },
+};
